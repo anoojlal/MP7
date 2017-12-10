80c83a97ff5590a306acf49f418e72ff2fd2017d
Anooj Lal <anoojl2@illinois.edu>
2017-12-09 21:21:11 -0600

    commit

--- src/Hangman.java
+++ src/Hangman.java
@@ -1,4 +1,19 @@
 
 public class Hangman {
 
+	private word;
+	private ArrayList<char> guessed;
+	private char[] display;
+	
+	public Hangman(final String word) {
+		this.word = word;
+		guessed = new ArrayList<char>;
+		display = new char[word.length()];
+		Arrays.fill(display, '_');
+	}
+	
+	public Hangman() {
+		//random word
+	}
+	
 }
