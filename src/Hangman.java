46f4bc2a3af259c6b1cb39249423a9daaed103b3
Thomas Yang <thomasy4@illinois.edu>
2017-12-09 21:43:15 -0600

    Add files via upload
    commit

--- src/Hangman.java
+++ src/Hangman.java
@@ -1,19 +1,25 @@
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Hangman {
 
-	private word;
-	private ArrayList<char> guessed;
-	private char[] display;
+	private String word;
+	private ArrayList<Character> guessed;
+	private static char[] display;
 	
 	public Hangman(final String word) {
 		this.word = word;
-		guessed = new ArrayList<char>;
+		guessed = new ArrayList<Character>();
 		display = new char[word.length()];
 		Arrays.fill(display, '_');
 	}
 	
 	public Hangman() {
 		//random word
+	}
+	
+	public static void play() {
+		System.out.println(display);
 	}
 	
 }
